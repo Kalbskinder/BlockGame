46b03886a3c0e0d3c7d0627abaa64eab521645ea
Kalbskinder <141642840+Kalbskinder@users.noreply.github.com>
2025-11-14 15:22:13 +0100

    Added new splash texts

--- libs/data/splashtexts.ts
+++ libs/data/splashtexts.ts
@@ -6,6 +6,10 @@ export const splashTexts: SplashTextEntry[] = [
     { text: "Three.js!", fontSize: 2.2 },
     { text: "crumb", fontSize: 2.2 },
     { text: "Hello World!", fontSize: 2.2 },
+    { text: "Kalbskinder", fontSize: 2.2 },
+    { text: "skinview3d", fontSize: 2.2 },
+    { text: "Browser Edition", fontSize: 1.9 },
+    { text: "Uhm... Guys?", fontSize: 2.0 },
     { text: "helloWorld(\"print\");", fontSize: 1.8 },
 ];
 
